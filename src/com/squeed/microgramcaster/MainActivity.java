052ab5c895ff543ff989c7d8d87fba36416a75f0
Erik Lupander <erik.lupander@qmatic.com>
2014-02-19 17:50:37 +0100

    Ported over some very basic untested code and settings from the hipstacaster project

--- src/com/squeed/microgramcaster/MainActivity.java
+++ src/com/squeed/microgramcaster/MainActivity.java
@@ -11,20 +11,62 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+
+import com.google.cast.ApplicationChannel;
+import com.google.cast.ApplicationMetadata;
+import com.google.cast.ApplicationSession;
+import com.google.cast.CastContext;
+import com.google.cast.CastDevice;
+import com.google.cast.MediaRouteAdapter;
+import com.google.cast.MediaRouteHelper;
+import com.google.cast.MediaRouteStateChangeListener;
+import com.google.cast.SessionError;
+
 import com.squeed.microgramcaster.server.WebServerService;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity implements MediaRouteAdapter  {
 
     private static final String TAG = "MainActivity";
+	
+	private static final String APP_NAME = "fc91668a-cf4b-4a18-9611-f2c120d0bf07_1";
+    private static final String PROTOCOL = "com.squeed.microgramcaster";
+	
+	private CastContext mCastContext;
+    private CastDevice mSelectedDevice;
+    private MediaRouter mMediaRouter;
+    private MediaRouteSelector mMediaRouteSelector;
+    private MediaRouter.Callback mMediaRouterCallback;
+	private ApplicationSession mSession;
+    private SessionListener mSessionListener;
+	
+	
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startWebServer();
-        
+        initMediaRouter() ;
         listVideoFiles();
     }
+	
+	private void initMediaRouter() {
+		mSessionListener = new SessionListener();
+        mMessageStream = new CustomHipstaCasterStream();
+
+        mCastContext = new CastContext(getApplicationContext());
+        MediaRouteHelper.registerMinimalMediaRouteProvider(mCastContext, this);
+        
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = MediaRouteHelper.buildMediaRouteSelector(
+                MediaRouteHelper.CATEGORY_CAST, APP_NAME, null);
+        mMediaRouterCallback = new MediaRouterCallback();
+	}
 
 
 	private void listVideoFiles() {
@@ -54,5 +96,72 @@ public class MainActivity extends Activity {
         this.stopService(webServerService);
         Log.i(TAG, "Stopped web server");
     }
+	
+	
+	
+	
+	/**
+     * An extension of the MessageStream with some local details.
+     */
+    private class CustomMicrogramCasterStream extends MicrogramCasterMessageStream {
+
+        /**
+         * Displays an error dialog.
+         */
+        @Override
+        protected void onError(String errorMessage) {
+        	//buildAlertDialog("Error", errorMessage);
+        }
+
+        /**
+         * Displays a message that the slideshow has ended.
+         */
+		@Override
+		protected void onSlideShowEnded() {
+			//buildAlertDialog("Message from ChromeCast", "Slideshow has ended");
+		}
+
+		/**
+		 * Updates the textView with the currently viewed photo from the slideshoe (n of m)
+		 */
+		@Override
+		protected void onCurrentSlideShowImageMessage(String message) {
+			//mInfoView.setText(message);
+		}		
+    }
+    
+    
+    
+
+
+    /**
+     * An extension of the MediaRoute.Callback so we can invoke our own onRoute selected/unselected
+     */
+    private class MediaRouterCallback extends MediaRouter.Callback {    	
+
+		@Override
+        public void onRouteSelected(MediaRouter router, android.support.v7.media.MediaRouter.RouteInfo route) {
+            Log.i(TAG, "onRouteSelected: " + route);
+            MainActivity.this.onRouteSelected(route);
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, android.support.v7.media.MediaRouter.RouteInfo route) {
+        	Log.i(TAG, "onRouteUnselected: " + route);
+            MainActivity.this.onRouteUnselected(route);
+        }
+    }
+
+    @Override
+    public void onDeviceAvailable(CastDevice device, String routeId,
+                                  MediaRouteStateChangeListener listener) {
+       // sLog.d("onDeviceAvailable: %s (route %s)", device, routeId);
+        setSelectedDevice(device);
+    }
+
+	
+	private boolean isCastSessionActive() {
+		return mSession != null && mSession.hasChannel() && mSession.hasStarted();
+	}	
     
 }
---
+++ src/com/squeed/microgramcaster/MicrogramCasterMessageStream.java
@@ -0,0 +1,144 @@
+package com.squeed.microgramcaster;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.google.cast.MessageStream;
+import com.squeed.chromecast.hipstacaster.grid.ImageItem;
+
+/**
+ * An abstract class which encapsulates control for sending and receiving messages to/from the receiver app.
+ * 
+ * Derived from the tic-tac-tie example at: https://github.com/googlecast/cast-android-tictactoe
+ */
+public abstract class MicrogramCasterMessageStream extends MessageStream {
+	private static final String TAG = MicrogramCasterMessageStream.class.getSimpleName();
+
+	private static final String HIPSTACASTER_NAMESPACE = "com.squeed.microgramcaster";
+
+	// Receivable event types
+	private static final String KEY_EVENT = "event";
+	private static final String KEY_ERROR = "error";
+	private static final String KEY_MESSAGE = "message";
+	
+	private static final String KEY_COMMAND = "command";
+	
+	private static final String KEY_TEXT = "text";
+	private static final String KEY_SLIDESHOW_ENDED = "slideshow_ended";
+	private static final String KEY_SLIDESHOW_CURRENT_IMAGE_MSG = "slideshow_current_image";
+
+
+	/**
+	 * Constructs a new HipstaCasterMessageStream with HIPSTACASTER_NAMESPACE as the namespace
+	 * used by the superclass.
+	 */
+	protected HipstaCasterMessageStream() {
+		super(HIPSTACASTER_NAMESPACE);
+	}
+
+	protected abstract void onSlideShowEnded();
+	protected abstract void onCurrentSlideShowImageMessage(String message);
+	protected abstract void onError(String errorMessage);
+	
+	/**
+	 * Sends image data for a single image to the cast device
+	 * @param title
+	 * 		Title of the photo.
+	 * @param url
+	 * 		The URL from where to load the actual image.
+	 * @param ownerName
+	 * 		Real name or username of the owner of the photo, as provided by the 3rd party photo service. (E.g. flickr)
+	 * @param description
+	 * 		Textual description of the photo. If null, the receiver app should handle that.
+	 */
+	public final void openPhotoOnChromecast(String title, String url, String ownerName, String description) {
+        try {
+            Log.d(TAG, "openPhotoOnChromecast: " + url);
+            JSONObject payload = new JSONObject();
+            payload.put(KEY_COMMAND, "viewphoto");
+            payload.put("fullsizeUrl", url);
+            payload.put("ownerName", ownerName);
+            payload.put("title", title);
+            payload.put("description", description);
+           
+            sendMessage(payload);
+        } catch (JSONException e) {
+            Log.e(TAG, "Cannot parse or serialize data for openPhotoOnChromecast", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to send openPhotoOnChromecast message", e);
+        } catch (IllegalStateException e) {
+            Log.e(TAG, "Message Stream is not attached", e);
+        }
+    }
+	
+	/**
+	 * Sends a full set of data for images to the cast device.
+	 * @param photoSet
+	 * 		Photo data.
+	 * @param offset
+	 * 		Supply a starting offset for the cast device to use for starting the slideshow at some none 0 index.
+	 */
+	public final void sendPhotoSetToChromecast(ArrayList<ImageItem> photoSet, int offset) {
+		try {
+            Log.d(TAG, "sendPhotoSetToChromecast");
+            JSONArray array = new JSONArray();
+            for(ImageItem imageItem : photoSet) {
+            	JSONObject payload = new JSONObject();
+            	
+                payload.put("fullsizeUrl", imageItem.getUrl());
+                payload.put("ownerName", imageItem.getOwnerName());
+                payload.put("title", imageItem.getTitle());
+                payload.put("description", imageItem.getDescription());
+                array.put(payload);
+            }
+            
+            JSONObject obj = new JSONObject();
+            obj.put(KEY_COMMAND, "slideshow");
+            obj.put("photoSet", array);
+            obj.put("offset", offset);
+           
+            sendMessage(obj);
+        } catch (JSONException e) {
+            Log.e(TAG, "Cannot parse or serialize data for sendPhotoSetToChromecast", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to send a sendPhotoSetToChromecast message", e);
+        } catch (IllegalStateException e) {
+            Log.e(TAG, "Message Stream is not attached", e);
+        }
+	}
+
+	@Override
+	public void onMessageReceived(JSONObject message) {
+		try {
+			Log.d(TAG, "onMessageReceived: " + message);
+			if (message.has(KEY_EVENT)) {
+				String event = message.getString(KEY_EVENT);
+				if (KEY_ERROR.equals(event)) {
+					Log.d(TAG, "ERROR");
+					try {
+						String errorMessage = message.getString(KEY_MESSAGE);
+						onError(errorMessage);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+				else if (KEY_SLIDESHOW_ENDED.equals(event)) {
+					onSlideShowEnded();
+				}
+				else if (KEY_SLIDESHOW_CURRENT_IMAGE_MSG.equals(event)) {
+					onCurrentSlideShowImageMessage(message.getString(KEY_TEXT));
+				}
+			} else {
+				Log.w(TAG, "Unknown message: " + message);
+			}
+		} catch (JSONException e) {
+			Log.w(TAG, "Message doesn't contain an expected key.", e);
+		}
+	}	
+}
