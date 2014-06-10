d6394f5eb99f6a08c806460e05ee25ced69da87e
Erik Lupander <erik.lupander@squeed.com>
2014-06-10 21:27:45 +0200

    Added UIL lib and Constants source file

---
+++ src/com/squeed/microgramcaster/Constants.java
@@ -0,0 +1,8 @@
+package com.squeed.microgramcaster;
+
+public interface Constants {
+	String DLNA_ITEM = "DLNA_ITEM";
+	String DLNA_BACK = "DLNA_BACK";
+	String DLNA_FOLDER = "DLNA_FOLDER";
+	String SMB_FILE = "SMB_FILE";
+}
